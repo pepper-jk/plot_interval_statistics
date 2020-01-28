ac990cc91b4f94702bc1daee917bb2b3e8aec0dd
pepper-jk <jens.keim@stud.tu-darmstadt.de>
2020-01-28 14:34:36 +0100

    move plot functions to library

---
+++ lib/plotting.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import development_matplotlib.paper_plots as pp
+
+def savepdfviasvg(fig, name, **kwargs):
+    import subprocess
+    fig.savefig(name + ".svg", format="svg", **kwargs)
+    incmd = ["inkscape", name + ".svg", "--export-pdf={}.pdf".format(name),
+                "--export-pdf-version=1.5"]  # "--export-ignore-filters",
+    subprocess.check_output(incmd)
+
+def plot(data: list(dict({str: dict}))=None, plot_name: str="bar") -> bool:
+    """
+    Plot dataset entropies.
+
+    :param data: a list of dictionaries containing entropies from multiple entropies
+    :param plot_name: name of the output file
+    :return: True on success and False on failure
+    """
+
+    # default data for testing
+    if data is None:
+        print("Error: no data")
+        return False
+
+    types = []
+    # FIXME: workaround, since data[0].keys() does not work
+    for key in data.keys():
+        types += list(data[key].keys())
+        break
+
+    # use the Garcia preset
+    pp.pre_paper_plot(True)
+
+    fig, ax = plt.subplots()
+
+    ind = np.arange(start=0, stop=2*(len(types)), step=2)
+    plt.xticks(ind)
+    width = 0.24
+    half = int(len(data)/2)
+    add = -half
+    bars = []
+    for values in data.values():
+        bars.append(ax.bar(ind+width*add, list(values.values()), width))
+        add = add + 1
+
+    # label
+    ylabel = "Entropy"
+    if plot_name.lower().find("normalized") != -1:
+        ylabel = "Normalized Entropy"
+    ax.set_ylabel(ylabel)
+    #ax.set_title(plot_name)
+    ax.set_xticklabels(types)
+
+    # use the Garcia preset
+    pp.post_paper_plot(change=True, bw_friendly=True, adjust_spines=False, sci_y=False)
+
+    # legend
+    ax.legend(bars, data.keys(), bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=4, mode="expand", borderaxespad=0.)
+
+    # plot
+    filename = plot_name.lower().replace(" ", "_")
+    savepdfviasvg(fig, filename, dpi=300, bbox_inches='tight')
+    #plt.savefig(filename, dpi=300, bbox_inches='tight', format='pdf')
+
+    return True
--- plot_interval_statistics.py
+++ plot_interval_statistics.py
@@ -1,12 +1,10 @@
 #! /usr/bin/env python3
 
 import csv
-import matplotlib.pyplot as plt
-import numpy as np
 import os
 import sys
 
-import development_matplotlib.paper_plots as pp
+import lib.plotting as plt
 
 
 def parse_interval_stats(file_path: str) -> dict({str: list}):
@@ -82,69 +80,6 @@ def extract(file_paths: list([str]), normalized: bool=False, novel: bool=False, 
 
     return data
 
-def savepdfviasvg(fig, name, **kwargs):
-    import subprocess
-    fig.savefig(name + ".svg", format="svg", **kwargs)
-    incmd = ["inkscape", name + ".svg", "--export-pdf={}.pdf".format(name),
-                "--export-pdf-version=1.5"]  # "--export-ignore-filters",
-    subprocess.check_output(incmd)
-
-def plot(data: list(dict({str: dict}))=None, plot_name: str="bar") -> bool:
-    """
-    Plot dataset entropies.
-
-    :param data: a list of dictionaries containing entropies from multiple entropies
-    :param plot_name: name of the output file
-    :return: True on success and False on failure
-    """
-
-    # default data for testing
-    if data is None:
-        print("Error: no data")
-        return False
-
-    types = []
-    # FIXME: workaround, since data[0].keys() does not work
-    for key in data.keys():
-        types += list(data[key].keys())
-        break
-
-    # use the Garcia preset
-    pp.pre_paper_plot(True)
-
-    fig, ax = plt.subplots()
-
-    ind = np.arange(start=0, stop=2*(len(types)), step=2)
-    plt.xticks(ind)
-    width = 0.24
-    half = int(len(data)/2)
-    add = -half
-    bars = []
-    for values in data.values():
-        bars.append(ax.bar(ind+width*add, list(values.values()), width))
-        add = add + 1
-
-    # label
-    ylabel = "Entropy"
-    if plot_name.lower().find("normalized") != -1:
-        ylabel = "Normalized Entropy"
-    ax.set_ylabel(ylabel)
-    #ax.set_title(plot_name)
-    ax.set_xticklabels(types)
-
-    # use the Garcia preset
-    pp.post_paper_plot(change=True, bw_friendly=True, adjust_spines=False, sci_y=False)
-
-    # legend
-    ax.legend(bars, data.keys(), bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=4, mode="expand", borderaxespad=0.)
-
-    # plot
-    filename = plot_name.lower().replace(" ", "_")
-    savepdfviasvg(fig, filename, dpi=300, bbox_inches='tight')
-    #plt.savefig(filename, dpi=300, bbox_inches='tight', format='pdf')
-
-    return True
-
 def main(argv):
     """
     Parsing, calculating and plotting average entropies from ID2T interval statistics files.
@@ -179,11 +114,11 @@ def main(argv):
     data_cum = extract(file_paths, cumulative=True)
 
     # plot statistics
-    result = plot(data, plot_name="Average Interval Entropy")
-    result = result and plot(data_norm, plot_name="Average Normalized Interval Entropy")
-    result = result and plot(data_novel, plot_name="Average Novelty Interval Entropy")
-    result = result and plot(data_novel_norm, plot_name="Average Normalized Novelty Interval Entropy")
-    result = result and plot(data_cum, plot_name="Average Cumulative Interval Entropy")
+    result = plt.plot(data, plot_name="Average Interval Entropy")
+    result = result and plt.plot(data_norm, plot_name="Average Normalized Interval Entropy")
+    result = result and plt.plot(data_novel, plot_name="Average Novelty Interval Entropy")
+    result = result and plt.plot(data_novel_norm, plot_name="Average Normalized Novelty Interval Entropy")
+    result = result and plt.plot(data_cum, plot_name="Average Cumulative Interval Entropy")
     if not result:
         return 1
 
