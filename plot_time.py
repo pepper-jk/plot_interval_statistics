7e68ac6ceed507cb1a7dbfa1b07bef0872bf053c
pepper-jk <jens.keim@stud.tu-darmstadt.de>
2020-01-28 15:30:21 +0100

    add comments on how to get time data from ID2T

--- plot_time.py
+++ plot_time.py
@@ -4,8 +4,22 @@ import sys
 
 import lib.plotting as plt
 
+# seconds it took to read statistics of a pcap
+# get with:
+# id2t -i <pcap> -ry -T     # regular
+# id2t -i <pcap> -ry -t -T  # with extra tests
+# num_pkts_mio is given by pcap, you need to provide pcaps with specific length manually
+# then read seconds from output: Loaded file statistics in 159. sec by PCAP file processor.
+# FORMAT: label: {num_pkts_mio: seconds}
 read_statistics = {'Regular statistics': {'11': 193, '23': 392, '57': 1058, '212': 4033},
                    'With extra tests': {'11': 258, '23': 558, '57': 1354, '212': 5090}}
+
+# seconds it took to generate a given amount of packets
+# get with:
+# id2t -i <pcap> -T -a DDoS attackers.count=10 packets.per-second=1000 bandwidth.max=100000
+# num_pkts ~= attackers.count * packets.per-second
+# read seconds from output: Generating attack packets... done. (total: 2000 pkts in 4.067694902420044 seconds.)
+# FORMAT: attack: {num_pkts: seconds}
 packets_generated = {'DDoS': {'10000': 15, '100000': 155, '1000000': 1231}}
 
 def main(argv):
